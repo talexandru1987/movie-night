780f20a46fc33fcc25c20d47683bbbffea108f52
Katarzyna Jaromin-Kaliciak <jarominyanni@gmail.com>
2022-05-12 20:27:41 +0100

    fixed navbar

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,6 @@
-const handleNavBarToggle = () => {
-  const navBurgerBtn = $(".navbar-burger");
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 
-  const toggleNavBar = () => {
-    // get the nav container id (the div to show and hide)
-    const navContainerId = navBurgerBtn.attr("data-target");
-    const navContainer = $(`#${navContainerId}`);
-
-    // toggle the class for hamburger button to show/hide
-    navBurgerBtn.toggleClass("is-active");
-
-    // toggle the class for nav container to show/hide
-    navContainer.toggleClass("is-active");
-  };
-
-  navBurgerBtn.click(toggleNavBar);
-};
-
-$(document).ready(() => {
-  handleNavBarToggle();
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
 });
