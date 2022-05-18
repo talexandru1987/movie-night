dce9eda1056f3b2f9795629f34e0147850814b74
Faisal Mohammed <40631753+faisal244@users.noreply.github.com>
2022-05-19 00:28:15 +0100

    Got movie details from omdb api to render in JS
    - Set up an an API key for a new OMDb API
    
    - Created a URL variable that passes the ID from the users selected search result into an OMDb API url, which returns an object containing lots of details about their chosen film, which can be seen in the console
    
    - used a template string to dynamically render info about the users selected film onto the page - currently have just the movie poster and movie title pulling through, now need to work on mapping more html elements to data from the api so more content can render dynamically on the page

--- assets/js/movie.js
+++ assets/js/movie.js
@@ -23,17 +23,20 @@ const readFromLocalStorage = (key, defaultValue) => {
 //create the options for the fetch request
 const options = {
   method: "GET",
-  headers: {
-    "X-RapidAPI-Host": "online-movie-database.p.rapidapi.com",
-    "X-RapidAPI-Key": apiKey,
-  },
+  // headers: {
+  //   "X-RapidAPI-Host": "online-movie-database.p.rapidapi.com",
+  //   "X-RapidAPI-Key": apiKey,
+  // },
 };
 
 //the function for the api call
 const fetchData = async (url, options = {}) => {
   try {
     if (mockData) {
-      const response = await fetch("./assets/data/dataReponseYear.json", options);
+      const response = await fetch(
+        "./assets/data/dataReponseYear.json",
+        options
+      );
       const data = await response.json();
       return data;
     } else {
@@ -62,6 +65,51 @@ const writeToLocalStorage = (key, value) => {
   localStorage.setItem(key, stringifiedValue);
 };
 
+const renderMovieInfo = (movie) => {
+  const movieContainer = `<section class="is-flex">
+    <div
+      class="columns is-half-desktop is-full-mobile is-half-tablet is-half-desktop is-half-widescreen is-half-fullhd"
+    >
+      <!-- Movie poster image - to be pulled from the imdb api-->
+
+      <div
+        class="column is-half-desktop is-full-mobile is-half-tablet is-half-desktop is-half-widescreen is-half-fullhd is-justify-content-flex-start"
+      >
+        <!-- <div class="column"> -->
+        <img
+          class="movie-image"
+          src="${movie.Poster}"
+        />
+      </div>
+
+      <!-- Movie Title and Synopisis - to be pulled from the imdb api-->
+      <div
+        class="has-text-centered column is-half is-spaced message-header is-justify-content-flex-end"
+      >
+        <!-- <div class="column is-half-desktop is-spaced message-header is-full-mobile is-half-tablet is-half-desktop is-half-widescreen is-half-fullhd"> -->
+        <!-- <div class="column is-half"> -->
+
+        <div>
+          <h1 id="movie-title" class="title is-1 has-text-white is-spaced">
+            ${movie.Title}
+          </h1>
+          <h1 id="movie-synopsis" class="subtitle is-3 has-text-white">
+          ${movie.Plot}
+           
+          </h1>
+        </div>
+
+        <!-- Buttons -->
+      </div>
+
+      <!-- Modal - hidden by default - shows youtube trailer in a popup when clicked -->
+      <!-- need to hook up the youtube api to this  -->
+    </div>
+  </section>`;
+
+  $("#main-container").append(movieContainer);
+};
+
 //code to execute when ready
 const onReady = async () => {
   const queryString = window.location.search;
@@ -69,54 +117,59 @@ const onReady = async () => {
   const id = urlParams.get("id");
 
   //create the url
-  let url = `${baseURL}/title/${id}`;
+  let url = `https://www.omdbapi.com/?i=${id}&plot=full&apikey=ae0076fc`;
 
   //call the api
-  const movies = await fetchData(url, options);
-  console.log(movies);
+  const movie = await fetchData(url, options);
+  console.log(movie);
+
+  renderMovieInfo(movie);
 };
 
 //check if document is ready
 $(document).ready(onReady);
 
-
-
 // JS for the trailer modal on movie package
 
 // const trailerButton = $(".trailer-button");
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   // Functions to open and close a modal
   function openModal($el) {
-    $el.classList.add('is-active');
+    $el.classList.add("is-active");
   }
   function closeModal($el) {
-    $el.classList.remove('is-active');
+    $el.classList.remove("is-active");
   }
   function closeAllModals() {
-    (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+    (document.querySelectorAll(".modal") || []).forEach(($modal) => {
       closeModal($modal);
     });
   }
   // Add a click event on buttons to open a specific modal
-  (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+  (document.querySelectorAll(".js-modal-trigger") || []).forEach(($trigger) => {
     const modal = $trigger.dataset.target;
     const $target = document.getElementById(modal);
-    $trigger.addEventListener('click', () => {
+    $trigger.addEventListener("click", () => {
       openModal($target);
     });
   });
   // Add a click event on various child elements to close the parent modal
-  (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
-    const $target = $close.closest('.modal');
-    $close.addEventListener('click', () => {
+  (
+    document.querySelectorAll(
+      ".modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button"
+    ) || []
+  ).forEach(($close) => {
+    const $target = $close.closest(".modal");
+    $close.addEventListener("click", () => {
       closeModal($target);
     });
   });
   // Add a keyboard event to close all modals
-  document.addEventListener('keydown', (event) => {
+  document.addEventListener("keydown", (event) => {
     const e = event || window.event;
-    if (e.keyCode === 27) { // Escape key
+    if (e.keyCode === 27) {
+      // Escape key
       closeAllModals();
     }
   });
